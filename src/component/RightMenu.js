c0057bc1d082fdf63b765d2c9fd7c2f217c061ba
ThaoHuynhD <thaohuynh34.678@gmail.com>
2024-03-20 14:33:34 +0700

    update right menu

--- src/component/RightMenu.js
+++ src/component/RightMenu.js
@@ -10,25 +10,27 @@ export default function RightMenu() {
     };
     return (
         <div className='menu'>
-            <ul className='fixed right-40 top-1/3 flex flex-col' id='mainNav'>
-                {headerTitle.map((item, index) => {
-                    const isActive = item.link === activeTitle;
-                    return <li key={index}>
-                        <a className={`menu-item p-2 w-36 cursor-pointer ${isActive ? 'active' : ''}`}
-                            href={item.link}
-                            onClick={() => handleTitleClick(item.link)}
-                        >
-                            {isVietnamese ? item.vntitle : item.title}</a>
-                    </li>
-                })}
-            </ul>
-            <div className='fixed right-40 top-2/3 -rotate-90'>
-                <a className='p-2 w-36 cursor-pointer hover:text-amber-300'
-                    href='#banner'
-                    onClick={() => handleTitleClick('#banner')}
-                >
-                    {isVietnamese ? 'Về đầu trang' : 'Back to top'}
-                    <i className="fa-solid fa-angles-right ml-2"></i></a>
+            <div className='fixed right-20 top-1/3 flex flex-col'>
+                <ul id='mainNav'>
+                    {headerTitle.map((item, index) => {
+                        const isActive = item.link === activeTitle;
+                        return <li key={index}>
+                            <a className={`menu-item p-2 w-36 cursor-pointer ${isActive ? 'active' : ''}`}
+                                href={item.link}
+                                onClick={() => handleTitleClick(item.link)}
+                            >
+                                {isVietnamese ? item.vntitle : item.title}</a>
+                        </li>
+                    })}
+                </ul>
+                <div className='-rotate-90 mt-40'>
+                    <a className='p-2 w-36 cursor-pointer hover:text-amber-300'
+                        href='#banner'
+                        onClick={() => handleTitleClick('#banner')}
+                    >
+                        {isVietnamese ? 'Về đầu trang' : 'Back to top'}
+                        <i className="fa-solid fa-angles-right ml-2"></i></a>
+                </div>
             </div>
         </div>
     )
